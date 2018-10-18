05ac8e3e80429e206fa4a7a7842eddaad94d2d50
ffsws <44257732+ffsws@users.noreply.github.com>
2018-10-18 15:22:12 +0200

    Update bench.php

--- bench.php
+++ bench.php
@@ -69,7 +69,7 @@
 	$functions = get_defined_functions();
         $hostname = gethostname();
 	$line = str_pad("-",38,"-");
-	echo "<pre>$line\n|".str_pad("PHP BENCHMARK SCRIPT",36," ",STR_PAD_BOTH)."|\n$line\nStart : ".date("Y-m-d H:i:s")."\nServer : {$_SERVER['SERVER_NAME']}@{$_SERVER['SERVER_ADDR']}\nPHP version : ".PHP_VERSION."\nPlatform : ".PHP_OS. "\nHost: {$hostname}\n$line\n";
+	echo "<pre>$line\n|".str_pad("PHP BENCHMARK SCRIPT",36," ",STR_PAD_BOTH)."|\n$line\nStart : ".date("Y-m-d H:i:s")."\nServer : {$_SERVER['SERVER_NAME']}@{$_SERVER['SERVER_ADDR']}\nPHP version : ".PHP_VERSION."\nPlatform : ".PHP_OS. "\nHost : {$hostname}\n$line\n";
 	foreach ($functions['user'] as $user) {
 		if (preg_match('/^test_/', $user)) {
 			$total += $result = $user();
