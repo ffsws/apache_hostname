a95c5d4ae4ff77ba77c767a92ad42f7381343e0c
Florian Fröhlich <44257732+ffsws@users.noreply.github.com>
2019-02-05 21:25:17 +0100

    Update and rename index.php to index2.php

--- index2.php
+++ index2.php
@@ -6,6 +6,15 @@ $hostname = gethostname();
 print $hostname;
 		$secondsWait = 0.1;
 header("Refresh:$secondsWait");
+		
+?>
+		
+<?php
+
+foreach (getallheaders() as $name => $value) {
+    echo "$name: $value\n";
+}
+
 ?>
 </b>
 	</body>
