4551e68855a05525f0e4827e3551185db8a4779b
Florian Fröhlich <44257732+ffsws@users.noreply.github.com>
2019-06-03 10:58:49 +0200

    Update index.php
    Background on Hostname

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
 <?php
 $hostname = gethostname();
 print $hostname;
+if (strpos($hostname, 'blue') !== false) {
+echo "<body style='background-color:blue'>";
+};
+if (strpos($hostname, 'green') !== false) {
+echo "<body style='background-color:green'>";
+};
 ?>
 </b>
 	</body>
